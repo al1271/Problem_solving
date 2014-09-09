c71e4e5581a1e2927c77c684e6b901c881d35ce7
alexander garza <alexzangarza@gmail.com>
2014-09-08 23:48:53 -0400

    finish
    doneeeeeeee

--- Day3 lab/Lab 3/js/main.js
+++ Day3 lab/Lab 3/js/main.js
@@ -1,29 +1,55 @@
 //alexander garza 09/08/14 section 0 day3LabExpressions
 
 //calculate number of slices eaten per person
+
 var pizzas = 10;
 var slices = 4;
 var people = 8;
+
 //multiply slices * pizzas then divide people to find slices per person
+
 var perPerson= (pizzas * slices) / people;
+
 //output perPerson
+
 console.log("Each person ate " + perPerson + " slices of pizza at the party");
 
 //Calculate number of slices for Sparky
+
 people = 10;
 pizzas = 4;
 slices = 8;
+
 //multiply slices * pizza then use the modulo on people
+
 var sparky = (slices * pizzas) % people;
 
 console.log("sparky got " + sparky + " slices of pizza");
 
 //Average shopping bill
 //declaring bill amounts
+
 var bills=[80,70,90,40,65];
+
 //getting the total and getting the average
+
 var total = bills[0] + bills[1] + bills[2] + bills[3] + bills[4];
-//using the total amount to find the avarage
+
+//using the total amount to find the average
+
 var avarage = total / 5;
 console.log(" you have spent a total of "+  total + " on groceries over 5 weeks. that is an avarage of " + avarage + " per week");
+
 //calculating the discount for an amount
+
+var original = 100;
+var percent = 50;
+var item = "Videogame";
+var taxPercent = .1;
+var percentConversion = percent / 100;
+
+//price of item with and without tax calculations
+
+var untaxed= original*percentConversion;
+var taxed = (untaxed * taxPercent) + untaxed;
+console.log(" your " + item + " was originally $" + original + " but after a " + percent + "% discount, it is now $" + untaxed + " without tax, and $" + taxed + " with tax ");
