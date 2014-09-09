ee3b429a2da5c0c45f43ad6edac73d425af053c4
alexander garza <alexzangarza@gmail.com>
2014-09-08 22:37:33 -0400

    Update
    update

--- Day3 lab/Lab 3/js/main.js
+++ Day3 lab/Lab 3/js/main.js
@@ -1,10 +1,23 @@
-//alexander garza 09/08/14 day3LabExpressions
-//calculate number of slices eaten per person
-var slices=180
-var people=15
-//divide slices / people
-var perPerson= slices / people
-//output perPerson
-console.log(" the amount of slices per person is ") + perPerson
+//alexander garza 09/08/14 section 0 day3LabExpressions
 
-//number two
+//calculate number of slices eaten per person
+var pizzas = 10;
+var slices = 4;
+var people = 8;
+//multiply slices * pizzas then divide people to find slices per person
+var perPerson= (pizzas * slices) / people;
+//output perPerson
+console.log("Each person ate " + perPerson + " slices of pizza at the party");
+
+//Calculate number of slices for Sparky
+people = 10;
+pizzas = 4;
+slices = 8;
+//multiply slices * pizza then use the modulo on people
+var sparky = (slices * pizzas) % people;
+
+console.log("sparky got " + sparky + " slices of pizza");
+
+//Average shopping bill
+//declaring bill amounts
+var bills=[80,70,90,40,65];
