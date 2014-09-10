ab97fd28c84fedb7fcbab1863c04d3e780657bbf
alexander garza <alexzangarza@gmail.com>
2014-09-10 17:59:43 -0400

    Finished all 3
    Done

--- Day 4/Expressions_Industry/Js/Main.js
+++ Day 4/Expressions_Industry/Js/Main.js
@@ -1,14 +1,16 @@
 //Alexander Garza 09/10/14 section 0 Expressions_Industry
 
-//Calculate
+//Calculate how many inches are in a 250x500 image with 120 dpi
 
+var width = 250;
+var height = 500;
+var dpi = 120;
 
+//using the following variables to calculate the inches of the image.
 
-
-//using the following variables to calculate the area.
-
-
+var inchW = width / dpi;
+var inchH = height / dpi;
 
 //using the output from the above to show the solutions
 
-console.log();
+console.log("The width of a 250x500 image is " + inchW + "inches and the height of it would be " + inchH + "inches" );
