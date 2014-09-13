a80545805960a619da0cc8d882a789a13aeae577
alexander garza <alexzangarza@gmail.com>
2014-09-12 22:06:00 -0400

    Finished lab
    finish

--- Day 5/Conditionals_worksheet/js/main.js
+++ Day 5/Conditionals_worksheet/js/main.js
@@ -3,13 +3,13 @@
 //#1
 var temp = Number(prompt("What temperature is it?"));
 //types of temperatures
-var temperatureType = prompt("Are you converting to celsius or fahrenheit");
+var temperatureType = prompt("Are you converting to celsius or fahrenheit").toLowerCase();
 //prompt asking what type of temperature
 
 if(temperatureType == "fahrenheit"){
-    console.log("the temperature is " + ((temp - 32) * 5 / 9) + "degrees Fahrenheit.")
+    console.log("the temperature is " + (temp * 9 / 5 + 32) + "degrees Fahrenheit.");
 } else if (temperatureType == "celsius") {
-    console.log("The temperature is " + (temp * 9 / 5 + 32) + " degrees Celsius.")
+    console.log("The temperature is " + ((temp - 32) * 5 / 9) + " degrees Celsius.");
 }
 
 
@@ -32,29 +32,30 @@ if(eUser === user && ePass === pass){
     //if user is correct but password is not
 }else if (eUser === user && ePass != pass) {
     //console says password does not match
-    console.log("Password does not match our records.")
+    console.log("Password does not match our records.");
 }else{
     //if user nor password match
-    console.log("User or password does not match our records please try again.")
+    console.log("User or password does not match our records please try again.");
 }
 
 
 //#3
 //time and age are asked through a prompt
-var time = prompt("Is it between the times of 3pm and 5pm(Yes or No)");
-var age = prompt("Are you under 10 or over 55? (Yes or No)");
+var time = prompt("Is it between the times of 3pm and 5pm(Yes or No)").toLowerCase();
+var age = prompt("Are you under 10 or over 55? (Yes or No)").toLowerCase();
 //ticket prices n=no discount d=discount
 var nTicket = 12;
 var dTicket = 7;
 //if time and age are in discount range
-if(time === "Yes" && age === "Yes"){
+if(time === "yes" && age === "yes"){
     console.log("Your ticket price is $" + dTicket );
-    //if time isn't but age is
-}else if(time === "No" && age === "Yes"){
-    console.log("Your ticket price is $" + dTicket)
-
-}else if(time === "Yes" && age === "No"){
-        console.log("Your ticket price is $" + dTicket)
+    //if time isn't but age is no discount price
+}else if(time === "no" && age === "yes"){
+    console.log("Your ticket price is $" + dTicket);
+    //if time is but age isn't discount price
+}else if(time === "yes" && age === "no"){
+        console.log("Your ticket price is $" + dTicket);
+        //if neither are true normal price
 }else{
-        console.log("Your ticket price is $" + nTicket)
+        console.log("Your ticket price is $" + nTicket);
     }
