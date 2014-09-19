d02b9822fda2eead1df0ba012781ce5f7456893b
alexander garza <alexzangarza@gmail.com>
2014-09-19 19:08:07 -0400

    update
    update

--- Day 7/Demo/Function/Js/Main.js
+++ Day 7/Demo/Function/Js/Main.js
@@ -22,35 +22,11 @@ console.log("there are " + totalFruit + " " + "apples.");
 
 
 
-var radius = Number(prompt("what is the radius of the circle?"));
-//using 3.14 for pi
-var pi = 3.14;
-//setting up the function call
-var circum = calculateCircum(2, pi, radius);
-//output once the function is run
-console.log("The circumference of the circle is" + circum);
-//calculating the circumference of the circle
-function calculateCircum( pi, radius){
-    //what the console log is going to spit out after calculation
-    return 2*pi*radius;
-
-
-
-//Stung!!! Calculating the amount of stings it'll take to kill an animal depending on its weight
-
-
+function calcArea (){
+    var width = 30;
+    var height = 20;
+    var area = width * height;
+    console.log(area)
 }
-//asking the user what the weight of the animal is
-var weight = Number(prompt("How much does the animal weigh?"));
-//Amount of stings itll take to kill an animal per pound
-var stingPound = 8.666666667;
-//setting up the function call
-var amountStings = calculateAmount(weight, stingPound);
-//setting up the output of the amount of stings itll take to kill the animal
-console.log("The amount of stings it'll take to kill this animal is" + amountStings);
-//calling the function
-function calculateAmount(weight,stingPound) {
-    //what the console log is going to spit out after calculation
-    return stingPound * weight;
-
-}
+calcArea();
+calcArea();
--- Day 8/Function worksheet/js/main.js
+++ Day 8/Function worksheet/js/main.js
@@ -1,4 +1,4 @@
-//
+//Alexander Garza 09/19/14 section 00
 
 //Circumference. Calculating the circumference of a circle depending on its radius.
 
@@ -8,18 +8,18 @@ var pi = 3.14;
 //setting up the function call
 var circum = calculateCircum(2, pi, radius);
 //output once the function is run
-console.log("The circumference of the circle is" + circum);
+console.log("The circumference of the circle is " + circum);
 //calculating the circumference of the circle
-function calculateCircum( pi, radius){
+function calculateCircum( pi, radius) {
     //what the console log is going to spit out after calculation
-    return 2*pi*radius;
+    return 2 * pi * radius;
+}
 
 
 
 //Stung!!! Calculating the amount of stings it'll take to kill an animal depending on its weight
 
 
-}
 //asking the user what the weight of the animal is
 var weight = Number(prompt("How much does the animal weigh?"));
 //Amount of stings itll take to kill an animal per pound
@@ -27,7 +27,7 @@ var stingPound = 8.666666667;
 //setting up the function call
 var amountStings = calculateAmount(weight, stingPound);
 //setting up the output of the amount of stings itll take to kill the animal
-console.log("The amount of stings it'll take to kill this animal is" + amountStings);
+console.log("The amount of stings it'll take to kill this animal is " + amountStings);
 //calling the function
 function calculateAmount(weight,stingPound) {
     //what the console log is going to spit out after calculation
