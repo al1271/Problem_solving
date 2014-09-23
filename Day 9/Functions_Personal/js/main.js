78155207164fdb0136541dd9a99b93077a0d62cc
alexander garza <alexzangarza@gmail.com>
2014-09-22 22:18:17 -0400

    finished with edits
    edits

--- Day 9/Functions_Personal/js/main.js
+++ Day 9/Functions_Personal/js/main.js
@@ -22,25 +22,25 @@ var thing= average(grade1,grade2,grade3);
 //Calculating the GPA fro the average grade
 if(thing > 94){
     //A+ GPA
-    console.log("You have a 4.0 GPA")
+    console.log("You have a 4.0 GPA");
 }else if(thing >= 87 && thing <= 94){
     //A GPA
-    console.log("You have a 3.7 GPA")
+    console.log("You have a 3.7 GPA");
     //B+ GPA
 }else if(thing < 87 && thing >= 80){
     //B GPA
-    console.log("You have a 3.3 GPA")
+    console.log("You have a 3.3 GPA");
     //C+ GPA
 }else if(thing > 80 && thing <= 73){
     //C GPA
-    console.log("You have a 3.0 GPA")
+    console.log("You have a 3.0 GPA");
     //D+ GPA
 }else if(thing <= 73 && thing >= 66){
     //D GPA
-    console.log("You have a 2.7 GPA")
+    console.log("You have a 2.7 GPA");
 }else if(thing <= 66 && thing >= 61){
     //F+ GPA
-    console.log("You have a 2.3 GPA")
+    console.log("You have a 2.3 GPA");
 }else if(thing <= 60 && thing >= 53){
     //from 2.3 and under the user has failed
     console.log("you have a 2.0 GPA and you have failed the semester")
