7da411f2ee3897b57c5bcafdcb8881e79e2031c4
alexander garza <alexzangarza@gmail.com>
2014-09-30 12:49:56 -0400

    q
    q

---
+++ Day 9/PWA/js/main.js
@@ -0,0 +1,32 @@
+//console.log(lowest(4,5,4,3,2,1,3,12,1,3,1,3,4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function lowest(a){
+    var lowNum = a[0];
+
+    for(var i=1; i< a.length; i++) {
+       if (a[i] < lowNum){
+           lowNum = a[i];
+       }
+    }
+    return lowNum
+}
+*/
